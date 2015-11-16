a03c19df7fc9b917ad3d5a07692fcf1972b02005
Pavel Kretov <firegurafiku@gmail.com>
2015-11-16 05:08:21 +0300

    FDTD/Parameters.hs: add indentation
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- src/Physics/ImplicitEMC/FDTD/Parameters.hs
+++ src/Physics/ImplicitEMC/FDTD/Parameters.hs
@@ -3,17 +3,17 @@ module Physics.ImplicitEMC.FDTD.Parameters where
 import qualified Physics.ImplicitEMC.DiscreteSequence as DQ
 import qualified Physics.ImplicitEMC.DiscreteSignal   as DS
 import qualified Physics.ImplicitEMC.FDTD.YeeLattice  as YL
-import qualified Graphics.Implicit.Definitions as ID
+import qualified Graphics.Implicit.Definitions        as ID
 
 data Parameters = Parameters {
-    lattice            :: YL.YeeLattice,
-    moments            :: DQ.DiscreteSequence,
-    excitationSignals  :: [DS.DiscreteSignal],
-    boundaryConditions :: [BoundaryCondition],
-    tfsfRegion         :: Maybe ID.Box3,
-    ffTransformRegion  :: Maybe ID.Box3,
-    localProbes        :: [LocalProbe],
-    farFieldProbes     :: [FarFieldProbe]
+        lattice            :: YL.YeeLattice,
+        moments            :: DQ.DiscreteSequence,
+        excitationSignals  :: [DS.DiscreteSignal],
+        boundaryConditions :: [BoundaryCondition],
+        tfsfRegion         :: Maybe ID.Box3,
+        ffTransformRegion  :: Maybe ID.Box3,
+        localProbes        :: [LocalProbe],
+        farFieldProbes     :: [FarFieldProbe]
 }
 
 data BoundaryCondition = BoundaryCondition
